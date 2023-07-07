f6cc7333e1db34ce07794826cdc50b4d8e1e32f7
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2023-07-07 12:54:58 +0530

    Added SliderDots

---
+++ lib/components/slider_dots.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:voicematch/constants/colors.dart';
+import 'package:voicematch/elements/div.dart';
+
+class SliderDots extends StatelessWidget {
+  final int total;
+  final int index;
+
+  final double? w;
+  final double? h;
+
+  const SliderDots({
+    Key? key,
+    required this.index,
+    required this.total,
+    this.w,
+    this.h,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      children: [
+        for (var i = 0; i < total; i++)
+          Div(
+            const [],
+            w: w ?? 12,
+            h: h ?? 12,
+            br: 12,
+            mh: 6,
+            bg: index == i ? colorPrimary : const Color(0xFFD9D9D9),
+          )
+      ],
+    );
+  }
+}
