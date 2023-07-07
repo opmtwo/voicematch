4e99f0406f17a71323f5af28af0af6f944fa3327
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2023-07-07 12:51:56 +0530

    Added options

---
+++ lib/constants/options.dart
@@ -0,0 +1,124 @@
+const List<Map<String, String>> genderOptions = [
+  {
+    'id': 'male',
+    'name': 'Male',
+  },
+  {
+    'id': 'female',
+    'name': 'Female',
+  },
+];
+
+const List<Map<String, String>> ageGroupOptions = [
+  {
+    'id': '18-24',
+    'name': '18-24',
+  },
+  {
+    'id': '25-34',
+    'name': '25-34',
+  },
+  {
+    'id': '35-44',
+    'name': '35-44',
+  },
+  {
+    'id': '45-54',
+    'name': '45-54',
+  },
+  {
+    'id': '55-64',
+    'name': '55-64',
+  },
+  {
+    'id': '65+',
+    'name': '65 and above',
+  },
+];
+
+const List<Map<String, String>> distanceOptions = [
+  {
+    'id': '0-5',
+    'name': '0km - 5km',
+  },
+  {
+    'id': '0-10',
+    'name': '0km - 10km',
+  },
+  {
+    'id': '0-20',
+    'name': '0km - 20km',
+  },
+  {
+    'id': '0-50',
+    'name': '0km - 50km',
+  },
+  {
+    'id': '0-100',
+    'name': '0km - 100km',
+  },
+  {
+    'id': '0-200',
+    'name': '0km - 200km',
+  },
+];
+
+const List<String> localeOptions = [
+  'English',
+  'Spanish',
+  'French',
+  'German',
+  'Chinese',
+  'Japanese',
+];
+
+const List<String> interestCreativityOptions = [
+  'Photography',
+  'Singing',
+  'Acting',
+  'Crafts',
+  'Dancing',
+  'Writing',
+  'Digital Art',
+  'Art',
+];
+
+const List<String> interestSportsOptions = [
+  'American football',
+  'Badminton',
+  'Baseball',
+  'Atheletics',
+  'Bowling',
+  'Boxing',
+];
+
+const List<String> interestVideoOptions = [
+  'Action',
+  'Documentaries',
+  'Anime',
+  'Comedy',
+  'Animated',
+  'Fantasy',
+  'Crime',
+];
+
+const List<String> interestMusicOptions = [
+  'Afro',
+  'Classical',
+  'Blues',
+  'Arab',
+  'Country',
+  'EDM',
+  'Electronic',
+];
+
+const List<String> interestTravellingOptions = [
+  'Camping',
+  'Beaches',
+  'Hiking Trips',
+  'Citybreaks',
+  'Surfing',
+  'Road Trips',
+];
+
+const List<String> interestPetOptions = ['Cats', 'Dogs', 'Rabbits'];
