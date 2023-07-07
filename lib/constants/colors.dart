552b734bdf827ca368fc6e3780aa91fd68fea035
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2023-07-07 12:51:44 +0530

    Added colors

---
+++ lib/constants/colors.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+const Color colorPrimary050 = Color(0xFFFEEAEF);
+const Color colorPrimary100 = Color(0xFFFECBD5);
+const Color colorPrimary200 = Color(0xFFEE959F);
+const Color colorPrimary300 = Color(0xFFE56B79);
+const Color colorPrimary400 = Color(0xFFF1435A);
+const Color colorPrimary500 = Color(0xFFF82641);
+const Color colorPrimary600 = Color(0xFFE81A40);
+const Color colorPrimary700 = Color(0xFFD60939);
+const Color colorPrimary800 = Color(0xFFC90031);
+const Color colorPrimary900 = Color(0xFFBB0025);
+
+// const Color colorSeondary050 = Color(0xFF00BF86);
+const Color colorSeondary050 = Color(0xFFCAF0F8);
+const Color colorSeondary100 = Color(0xFF90E0EF);
+const Color colorSeondary200 = Color(0xFF00B4D8);
+const Color colorSeondary500 = Color(0XFF0077B6);
+const Color colorSeondary900 = Color(0xFFCAF0F8);
+
+const Color colorGrey200 = Color(0xFFE6E4E7);
+const Color colorGrey900 = Color(0xFF020202);
+
+const Color colorPrimary = colorPrimary500;
+const Color colorPrimaryVariant = colorPrimary700;
+
+const Color colorSeondary = Color(0xFF1AE8C2);
+const Color colorSeondaryVariant = colorSeondary200;
+
+const Color colorWhite = Colors.white;
+const Color colorBlack = Colors.black;
+const Color colorGrey = Color(0xFF767676);
+const Color colorTransparent = Colors.transparent;
+
+const Color colorOnPrimaryHighEmphasis = Color(0x00FFFFFF);
+const Color colorOnPrimaryMediumEmphasis = Color(0xBDFFFFFF);
+const Color colorOnPrimaryDisabled = Color(0x61FFFFFF);
+
+Color colorOnSurfaceHighEmphasis = const Color(0xFF000000).withOpacity(0.87);
+Color colorOnSurfaceMediumEmphasis = const Color(0xFF000000).withOpacity(0.60);
+Color colorOnSurfaceDisabled = const Color(0xFF000000).withOpacity(0.38);
+
+const Color colorBackground = Color(0xFFFFFFFF);
+const Color colorSurface = Color(0xFFFFFFFF);
+
+const Color colorError = Color(0xFFB00020);
+const Color colorOnPrimary = Color(0xFFFFFFFF);
+
+const Color colorOnSecondary = Color(0xFF000000);
+const Color colorOnBackground = Color(0xFF000000);
+const Color colorOnSurface = Color(0xFF000000);
+
+const Color colorOnError = Color(0xFFFFFFFF);
