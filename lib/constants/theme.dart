b337ea55cde56a948f51713fd8cef2b1943fb1eb
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2023-07-07 12:52:05 +0530

    Added theme config

---
+++ lib/constants/theme.dart
@@ -0,0 +1,7 @@
+const double gap = 20;
+const double gapTop = gap * 3;
+const double gapBottom = gap * 2;
+const double gapHorizontal = gap * 1.5;
+const double radius = 4;
+const double radiusMedium = radius * 2;
+const double radiusLarge = radius * 5;
