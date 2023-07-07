78c43da6cca595136a364bb185d82a07ea325b5b
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2023-07-07 12:52:46 +0530

    Added Loader

---
+++ lib/components/loader.dart
@@ -0,0 +1,58 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:voicematch/components/slider_dots.dart';
+import 'package:voicematch/elements/div.dart';
+
+class Loader extends StatefulWidget {
+  const Loader({super.key});
+
+  @override
+  State<Loader> createState() => _LoaderState();
+}
+
+/// AnimationControllers can be created with `vsync: this` because of TickerProviderStateMixin.
+class _LoaderState extends State<Loader> with TickerProviderStateMixin {
+  _LoaderState();
+
+  // timer to control loading dots
+  late Timer timer;
+
+  int index = 0;
+
+  @override
+  initState() {
+    super.initState();
+    timer = Timer.periodic(const Duration(milliseconds: 500), (timer) async {
+      if (!mounted) {
+        timer.cancel();
+        return;
+      }
+      setState(() {
+        index = (index + 1) % 3;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Div(
+      [
+        Image.asset(
+          'assets/images/logo.png',
+          width: 64,
+        ),
+        Div(
+          [
+            SliderDots(
+              total: 3,
+              index: index,
+              w: 8,
+              h: 8,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
