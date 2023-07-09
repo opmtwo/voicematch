9b017f82ce405ae4635d238867c54bd96879c6a0
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2023-07-09 16:56:06 +0530

    Added Header

---
+++ lib/components/header.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:voicematch/constants/colors.dart';
+import 'package:voicematch/constants/theme.dart';
+import 'package:voicematch/elements/div.dart';
+import 'package:voicematch/form/fab_button.dart';
+import 'package:voicematch/icons/icon_left.dart';
+
+class Header extends StatelessWidget {
+  final Color? bg;
+  final List<Widget> children;
+
+  final bool? hasPrev;
+  final Widget? prevIcon;
+  final VoidCallback? onPrev;
+
+  final bool? hasNext;
+  final Widget? nextIcon;
+  final VoidCallback? onNext;
+
+  const Header({
+    Key? key,
+    required this.children,
+    this.bg,
+    this.hasPrev,
+    this.prevIcon,
+    this.onPrev,
+    this.hasNext,
+    this.nextIcon,
+    this.onNext,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Div(
+      [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Div(
+              [
+                Visibility(
+                  visible: hasPrev == true,
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: FabButton(
+                      // alignment: Alignment.centerLeft,
+                      prevIcon ??
+                          SvgPicture.string(
+                            iconLeft(),
+                            width: 24,
+                          ),
+                      bg: Colors.transparent,
+                      onPress: onPrev,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              flex: 1,
+              child: Column(
+                children: children,
+              ),
+            ),
+            Div(
+              [
+                Visibility(
+                  visible: hasNext == true,
+                  child: Align(
+                    // alignment: Alignment.topRight,
+                    child: FabButton(
+                      alignment: Alignment.centerRight,
+                      nextIcon ??
+                          const Icon(
+                            Icons.arrow_left_rounded,
+                            color: colorWhite,
+                          ),
+                      bg: Colors.transparent,
+                      onPress: onNext,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ],
+      bg: bg,
+      pb: gap,
+      ph: gap,
+    );
+  }
+}
