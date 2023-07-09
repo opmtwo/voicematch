64ed40a47404c6d74601a52f37ba1905430f035f
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2023-07-09 16:59:27 +0530

    Added and updated icons

--- lib/icons/icon_pause.dart
+++ lib/icons/icon_pause.dart
@@ -5,7 +5,7 @@ String iconPause({Color? code = const Color(0xFF000000)}) {
   return ("""
 <svg viewBox="0 0 16 20" fill="none" xmlns="http://www.w3.org/2000/svg">
   <g clip-path="url(#clip0_801_1507)">
-    <rect width="6" height="20" rx="2" fill="white"/>
+    <rect width="6" height="20" rx="2" fill="${colorToHex(code)}"/>
     <rect x="9.99939" y="0.000976562" width="6" height="20" rx="2" fill="${colorToHex(code)}"/>
   </g>
   <defs>
---
+++ lib/icons/icon_wave_alt.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:voicematch/utils/color_utils.dart';
+
+String iconWaveAlt({Color? code = const Color(0xFFC3D1FF)}) {
+  return ("""
+<svg viewBox="0 0 192 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<rect x="14" y="4.19531" width="3" height="25.6083" rx="1.5" fill="${colorToHex(code)}" />
+	<rect x="21" y="9.88281" width="3" height="14.2269" rx="1.5" fill="${colorToHex(code)}" />
+	<rect x="28" y="13.6797" width="3" height="6.6392" rx="1.5" fill="${colorToHex(code)}" />
+	<rect x="35" y="6.09375" width="3" height="21.8145" rx="1.5" fill="${colorToHex(code)}" />
+	<rect x="42" y="9.88281" width="3" height="14.2269" rx="1.5" fill="${colorToHex(code)}" />
+	<rect x="49" y="12.7344" width="3" height="8.53611" rx="1.5" fill="${colorToHex(code)}" />
+	<rect x="56" y="9.88281" width="3" height="14.2269" rx="1.5" fill="${colorToHex(code)}" />
+	<rect x="63" y="5.14453" width="3" height="23.7114" rx="1.5" fill="${colorToHex(code)}" />
+	<rect x="70" y="0.402344" width="3" height="33.196" rx="1.5" fill="${colorToHex(code)}" />
+	<rect x="77" y="8.9375" width="3" height="16.1238" rx="1.5" fill="${colorToHex(code)}" />
+	<rect x="84" y="11.7812" width="3" height="10.433" rx="1.5" fill="${colorToHex(code)}" />
+	<rect x="91" y="13.6797" width="3" height="6.6392" rx="1.5" fill="${colorToHex(code)}" />
+	<rect x="98" y="7.03906" width="3" height="19.9176" rx="1.5" fill="${colorToHex(code)}" />
+	<rect x="105" y="3.24609" width="3" height="27.5053" rx="1.5" fill="${colorToHex(code)}" />
+	<rect x="112" y="8.9375" width="3" height="16.1238" rx="1.5" fill="${colorToHex(code)}" />
+	<rect x="119" y="11.7812" width="3" height="10.433" rx="1.5" fill="${colorToHex(code)}" />
+	<rect x="126" y="6.09375" width="3" height="21.8145" rx="1.5" fill="${colorToHex(code)}" />
+	<rect x="133" y="10.8359" width="3" height="12.3299" rx="1.5" fill="${colorToHex(code)}" />
+	<rect x="140" y="12.7344" width="3" height="8.53611" rx="1.5" fill="${colorToHex(code)}" />
+	<rect x="147" y="4.19531" width="3" height="25.6083" rx="1.5" fill="${colorToHex(code)}" />
+	<rect x="154" y="12.7344" width="3" height="8.53611" rx="1.5" fill="${colorToHex(code)}" />
+	<rect x="161" y="7.03906" width="3" height="19.9176" rx="1.5" fill="${colorToHex(code)}" />
+	<rect x="168" y="12.7344" width="3" height="8.53611" rx="1.5" fill="${colorToHex(code)}" />
+	<rect x="175" y="7.03906" width="3" height="19.9176" rx="1.5" fill="${colorToHex(code)}" />
+	<rect x="182" y="12.7344" width="3" height="8.53611" rx="1.5" fill="${colorToHex(code)}" />
+	<rect x="189" y="13.6797" width="3" height="6.6392" rx="1.5" fill="${colorToHex(code)}" />
+	<rect x="7" y="8.9375" width="3" height="15.1753" rx="1.5" fill="${colorToHex(code)}" />
+	<rect y="13.6797" width="3" height="6.6392" rx="1.5" fill="${colorToHex(code)}" />
+</svg>
+""");
+}
