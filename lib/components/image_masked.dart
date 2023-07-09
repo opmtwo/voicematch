27fb470807da3288c21d394edc7e5dbd94065e62
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2023-07-09 09:17:22 +0530

    Added ImageMasked

---
+++ lib/components/image_masked.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_html/shims/dart_ui_real.dart';
+
+class ImageMasked extends StatelessWidget {
+  final String url;
+  final double? width;
+  final double? height;
+  final double? sigmaX;
+  final double? sigmaY;
+
+  const ImageMasked({
+    Key? key,
+    required this.url,
+    this.width,
+    this.height,
+    this.sigmaX,
+    this.sigmaY,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(100),
+      child: Stack(
+        children: [
+          Image.asset(
+            url,
+            width: width,
+            height: height,
+            fit: BoxFit.cover,
+          ),
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(
+                sigmaX: sigmaX ?? 8,
+                sigmaY: sigmaX ?? 8,
+              ),
+              child: Container(
+                color: Colors.transparent,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
