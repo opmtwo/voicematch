ad7a978341b92582da99efea64ee524ce970bc61
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2023-07-10 20:22:37 +0530

    Completed sign in screen

--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -2,7 +2,9 @@ import 'dart:async';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
+import 'package:localstorage/localstorage.dart';
 import 'package:voicematch/components/app_links.dart';
 import 'package:voicematch/components/logo.dart';
 import 'package:voicematch/constants/colors.dart';
@@ -11,7 +13,10 @@ import 'package:voicematch/elements/div.dart';
 import 'package:voicematch/elements/p.dart';
 import 'package:voicematch/form/button.dart';
 import 'package:voicematch/form/checkbox.dart';
+import 'package:voicematch/form/fab_button.dart';
 import 'package:voicematch/form/input.dart';
+import 'package:voicematch/icons/icon_eye_close.dart';
+import 'package:voicematch/icons/icon_eye_open.dart';
 import 'package:voicematch/layouts/app_layout.dart';
 import 'package:voicematch/router.dart';
 import 'package:voicematch/utils/user_utils.dart';
@@ -31,9 +36,18 @@ class SignInScreenState extends State<SignInScreen> {
   final TextEditingController passwordController = TextEditingController();
   String? passwordError;
 
+  // remember?
   bool isRemember = false;
 
+  // busy and erro
+  bool? isBusy;
   String? error;
+
+  // password input masked?
+  bool isPasswordSecure = true;
+
+  // local storage - used to store badge counter
+  final storage = LocalStorage('voicematch.json');
 
   // Validate form
   Future<bool> isFormValid() async {
@@ -60,6 +74,7 @@ class SignInScreenState extends State<SignInScreen> {
   Future<void> onSubmit() async {
     EasyLoading.show(status: 'Loading');
 
+    // validate form
     bool isValid = await isFormValid();
     if (!isValid) {
       safePrint('Form validation failed');
@@ -67,20 +82,49 @@ class SignInScreenState extends State<SignInScreen> {
       return;
     }
 
+    // get form data
     String username = usernameController.text;
     String password = passwordController.text;
 
+    // try and sign in
     try {
+      // check if user is already signed in
       final session = await Amplify.Auth.fetchAuthSession();
       final isSignedIn = session.isSignedIn;
+
+      // try and sign in
       if (!isSignedIn) {
-        await Amplify.Auth.signIn(
+        SignInResult result = await Amplify.Auth.signIn(
           username: username,
           password: password,
         );
+
+        // set remember option in local storage
+        // will be used to sign out if remember option is set to false
+        // this is done in main.dart file
+        await storage.setItem('isRemember', isRemember);
       }
-      Get.toNamed(Routes.home);
+
+      // check if profile setup is pending
+      List<AuthUserAttribute> attributes =
+          await Amplify.Auth.fetchUserAttributes();
+      String isSetupDone = attributes
+              .firstWhereOrNull(
+                (element) =>
+                    element.userAttributeKey ==
+                    const CognitoUserAttributeKey.custom(
+                        'custom:is_setup_done'),
+              )
+              ?.value ??
+          '';
+
+      // clear history and navigate
+      Get.offNamedUntil(
+        isSetupDone == 'yes' ? Routes.home : Routes.setupIntro,
+        (route) => false,
+      );
     } on AuthException catch (e) {
+      // user confirmation is pending
       if (e.message.contains('not confirmed') ||
           e.message.contains('no registered')) {
         await Amplify.Auth.resendSignUpCode(username: usernameController.text);
@@ -90,11 +134,13 @@ class SignInScreenState extends State<SignInScreen> {
           'email': username,
           'password': password,
         });
-      } else {
-        setState(() {
-          error = e.message;
-        });
+        return;
       }
+
+      // something went wrong
+      setState(() {
+        error = e.message;
+      });
     }
 
     // dismiss loader
@@ -158,19 +204,47 @@ class SignInScreenState extends State<SignInScreen> {
                         bw: 0,
                         bg: colorWhite,
                         fg: colorBlack,
+                        error: usernameError,
+                        errorFg: colorPrimary100,
                       ), //
                     ],
                     mb: gap,
                   ),
                   Div(
                     [
-                      Input(
-                        passwordController,
-                        placeholder: '*Password',
-                        bw: 0,
-                        bg: colorWhite,
-                        fg: colorBlack,
-                      ), //
+                      Stack(
+                        children: [
+                          Input(
+                            passwordController,
+                            placeholder: '*Password',
+                            bw: 0,
+                            bg: colorWhite,
+                            fg: colorBlack,
+                            isPassword: isPasswordSecure,
+                            error: passwordError,
+                            errorFg: colorPrimary100,
+                          ),
+                          Positioned(
+                            top: 0,
+                            right: 0,
+                            child: FabButton(
+                              SvgPicture.string(
+                                isPasswordSecure
+                                    ? iconEyeClose()
+                                    : iconEyeOpen(),
+                                // width: 500,
+                                height: 24,
+                              ),
+                              bg: colorTransparent,
+                              onPress: () {
+                                setState(() {
+                                  isPasswordSecure = !isPasswordSecure;
+                                });
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
                     ],
                     mb: gap,
                   ),
