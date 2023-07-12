6f042ecbe07f3e771071432406ca65665497b071
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2023-07-12 13:06:51 +0530

    Added redirectUser util

--- lib/utils/user_utils.dart
+++ lib/utils/user_utils.dart
@@ -1,4 +1,7 @@
+import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:email_validator/email_validator.dart';
+import 'package:get/get.dart';
+import 'package:voicematch/router.dart';
 
 bool isPasswordValid(String value) {
   RegExp regex = RegExp(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[\S]{8,}$');
@@ -10,4 +13,25 @@ bool isPasswordValid(String value) {
 
 bool isEmailValid(String value) {
   return EmailValidator.validate(value);
+}
+
+Future<void> redirectUser() async {
+  List<AuthUserAttribute> attributes = await Amplify.Auth.fetchUserAttributes();
+  String isSetupDone = attributes
+          .firstWhereOrNull((element) =>
+              element.userAttributeKey ==
+              const CognitoUserAttributeKey.custom('custom:is_setup_done'))
+          ?.value ??
+      '';
+  if (isSetupDone == 'true') {
+    Get.offNamedUntil(
+      Routes.matchesIndex,
+      (route) => false,
+    );
+  } else {
+    Get.offNamedUntil(
+      Routes.setupIntro,
+      (route) => false,
+    );
+  }
 }
