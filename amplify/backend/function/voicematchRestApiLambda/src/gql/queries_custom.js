60ee3d278f74de05eea1d0a63c1ea7798e564d99
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2023-07-13 20:38:04 +0530

    Added getConnection to queries custom

--- amplify/backend/function/voicematchRestApiLambda/src/gql/queries_custom.js
+++ amplify/backend/function/voicematchRestApiLambda/src/gql/queries_custom.js
@@ -83,3 +83,78 @@ exports.listConnectionByUserId = /* GraphQL */ `
 		}
 	}
 `;
+
+exports.getConnection = /* GraphQL */ `
+	query GetConnection($id: ID!) {
+		getConnection(id: $id) {
+			id
+			owner
+			chatId
+			userId
+			memberId
+			member {
+				id
+				owner
+				email
+				name
+				givenName
+				familyName
+				pictureNormal
+				pictureMasked
+				gender
+				lookingFor
+				ageRange
+				distance
+				locale
+				interestCreativity
+				interestSports
+				interestVideo
+				interestMusic
+				interestTravelling
+				interestPet
+				introId
+				intro {
+					id
+					owner
+					userId
+					duration
+					key
+					url
+					createdAt
+					updatedAt
+					__typename
+				}
+				isSetupDone
+				searchTerm
+				createdAt
+				updatedAt
+				__typename
+			}
+			onlinePresence {
+				id
+				owner
+				status
+				lastSeenAt
+				createdAt
+				updatedAt
+				__typename
+			}
+			isSender
+			isReceiver
+			isAccepted
+			isDeclined
+			isBlocked
+			isMuted
+			isPinned
+			acceptedAt
+			declinedAt
+			blockedAt
+			mutedAt
+			pinnedAt
+			matchPercentage
+			createdAt
+			updatedAt
+			__typename
+		}
+	}
+`;
