669f5cc291eb657c4ab45bc07d99ae1ab5ac1fcc
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2023-07-13 20:42:59 +0530

    Added iconHome

---
+++ lib/icons/icon_home.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:voicematch/utils/color_utils.dart';
+
+String iconHome({Color? code = const Color(0xFF000000)}) {
+  return ("""
+<svg viewBox="0 0 64 64"
+    xmlns="http://www.w3.org/2000/svg">
+    <path d="M62.33,29.74,32,2.35,1.67,29.74.33,28.26l31-28a1,1,0,0,1,1.34,0l31,28Z"/>
+    <path d="M56,64H8a1,1,0,0,1-1-1V32H9V62H55V32h2V63A1,1,0,0,1,56,64Z"/>
+</svg>
+""");
+}
