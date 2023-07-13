198a95c9f88d18b07e28e902bf505db99a232364
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2023-07-13 11:58:16 +0530

    Added ProfilePic

---
+++ lib/components/profile_pic.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:voicematch/elements/div.dart';
+
+class ProfilePic extends StatelessWidget {
+  final String src;
+  final bool? isLocal;
+
+  final double? w;
+  final double? h;
+
+  const ProfilePic({
+    Key? key,
+    required this.src,
+    this.isLocal,
+    this.w,
+    this.h,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final double width = w ?? 48;
+    final double height = h ?? w ?? 48;
+    return Div(
+      [
+        if (isLocal == false)
+          ClipRRect(
+            borderRadius: BorderRadius.circular(100),
+            child: Image.network(
+              src,
+              width: width,
+              height: height,
+            ),
+          ),
+        if (isLocal == true)
+          ClipRRect(
+            borderRadius: BorderRadius.circular(100),
+            child: Image.asset(
+              src,
+              width: width,
+              height: height,
+            ),
+          ),
+      ],
+      w: width,
+      h: height,
+    );
+  }
+}
