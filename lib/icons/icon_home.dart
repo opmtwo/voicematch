10ee88427105e81febe81f5278b41edf0d90dd82
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2023-07-13 21:11:23 +0530

    Added and updated icons

--- lib/icons/icon_home.dart
+++ lib/icons/icon_home.dart
@@ -3,10 +3,11 @@ import 'package:voicematch/utils/color_utils.dart';
 
 String iconHome({Color? code = const Color(0xFF000000)}) {
   return ("""
-<svg viewBox="0 0 64 64"
+<svg viewBox="0 0 32 32"
     xmlns="http://www.w3.org/2000/svg">
-    <path d="M62.33,29.74,32,2.35,1.67,29.74.33,28.26l31-28a1,1,0,0,1,1.34,0l31,28Z"/>
-    <path d="M56,64H8a1,1,0,0,1-1-1V32H9V62H55V32h2V63A1,1,0,0,1,56,64Z"/>
+    <g id="home">
+        <path d="M29.71,15.29l-3-3h0l-10-10a1,1,0,0,0-1.42,0l-10,10h0l-3,3a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0L5,15.41V29a1,1,0,0,0,1,1H26a1,1,0,0,0,1-1V15.41l1.29,1.3a1,1,0,0,0,1.42,0A1,1,0,0,0,29.71,15.29ZM25,28H7V13.41l9-9,9,9Z"/>
+    </g>
 </svg>
 """);
 }
---
+++ lib/icons/icon_sign_out.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:voicematch/utils/color_utils.dart';
+
+String iconSignOut({Color? code = const Color(0xFF000000)}) {
+  return ("""
+<svg viewBox="0 0 128 128" xml:space="preserve">
+    <g>
+        <polygon points="91,119 9,119 9,9 91,9 91,1 1,1 1,127 91,127  "/>
+        <rect height="8" width="8" x="40" y="60"/>
+        <polygon points="90.8,96.8 123.7,64 90.8,31.2 85.2,36.8 108.3,60 58,60 58,68 108.3,68 85.2,91.2  "/>
+    </g>
+</svg>
+""");
+}
