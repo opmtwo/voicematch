64e7f0bf76a8e81d7fb43b75b616ca92e78da117
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2023-07-13 11:57:30 +0530

    Added queries custom

---
+++ amplify/backend/function/voicematchRestApiLambda/src/gql/queries_custom.js
@@ -0,0 +1,85 @@
+exports.listConnectionByUserId = /* GraphQL */ `
+	query ListConnectionByUserId(
+		$userId: ID!
+		$createdAt: ModelStringKeyConditionInput
+		$sortDirection: ModelSortDirection
+		$filter: ModelConnectionFilterInput
+		$limit: Int
+		$nextToken: String
+	) {
+		listConnectionByUserId(userId: $userId, createdAt: $createdAt, sortDirection: $sortDirection, filter: $filter, limit: $limit, nextToken: $nextToken) {
+			items {
+				id
+				owner
+				chatId
+				userId
+				memberId
+				member {
+					id
+					owner
+					email
+					name
+					givenName
+					familyName
+					pictureNormal
+					pictureMasked
+					gender
+					lookingFor
+					ageRange
+					distance
+					locale
+					interestCreativity
+					interestSports
+					interestVideo
+					interestMusic
+					interestTravelling
+					interestPet
+					introId
+					intro {
+						id
+						owner
+						userId
+						duration
+						key
+						url
+						createdAt
+						updatedAt
+						__typename
+					}
+					isSetupDone
+					searchTerm
+					createdAt
+					updatedAt
+					__typename
+				}
+				onlinePresence {
+					id
+					owner
+					status
+					lastSeenAt
+					createdAt
+					updatedAt
+					__typename
+				}
+				isSender
+				isReceiver
+				isAccepted
+				isDeclined
+				isBlocked
+				isMuted
+				isPinned
+				acceptedAt
+				declinedAt
+				blockedAt
+				mutedAt
+				pinnedAt
+				matchPercentage
+				createdAt
+				updatedAt
+				__typename
+			}
+			nextToken
+			__typename
+		}
+	}
+`;
