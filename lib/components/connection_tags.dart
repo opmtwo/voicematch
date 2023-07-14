9b3aa39736b204b2f9fb6c462ce87be1bb2c79db
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2023-07-14 11:30:25 +0530

    Updated ConnectionTags - added support for matching display

--- lib/components/connection_tags.dart
+++ lib/components/connection_tags.dart
@@ -4,15 +4,18 @@ import 'package:voicematch/constants/theme.dart';
 import 'package:voicematch/constants/types.dart';
 import 'package:voicematch/elements/div.dart';
 import 'package:voicematch/elements/p.dart';
+import 'package:voicematch/utils/helper_utils.dart';
 
 class ConnectionTags extends StatelessWidget {
   final String label;
   final ConnectionModel item;
+  final bool? isMatchOnly;
 
   const ConnectionTags({
     Key? key,
     required this.label,
     required this.item,
+    this.isMatchOnly,
   }) : super(key: key);
 
   @override
@@ -21,52 +24,76 @@ class ConnectionTags extends StatelessWidget {
     final member = item.member;
 
     if (member.interestCreativity != null) {
-      options.add({
-        'id': 'interestCreativity',
-        'value': member.interestCreativity as String,
-        'bg': colorSeondary,
-        'fg': colorBlack,
-      });
+      String userValue = slugify(item.user?.interestCreativity ?? '');
+      String memberValue = slugify(item.member.interestCreativity ?? '');
+      if (isMatchOnly != true || userValue == memberValue) {
+        options.add({
+          'id': 'interestCreativity',
+          'value': member.interestCreativity as String,
+          'bg': colorSeondary,
+          'fg': colorBlack,
+        });
+      }
     }
     if (member.interestSports != null) {
-      options.add({
-        'id': 'interestSports',
-        'value': member.interestSports as String,
-        'bg': colorSeondary,
-        'fg': colorBlack,
-      });
+      String userValue = slugify(item.user?.interestSports ?? '');
+      String memberValue = slugify(item.member.interestSports ?? '');
+      if (isMatchOnly != true || userValue == memberValue) {
+        options.add({
+          'id': 'interestSports',
+          'value': member.interestSports as String,
+          'bg': colorSeondary,
+          'fg': colorBlack,
+        });
+      }
     }
     if (member.interestVideo != null) {
-      options.add({
-        'id': 'interestVideo',
-        'value': member.interestVideo as String,
-        'bg': colorSeondary,
-        'fg': colorBlack,
-      });
+      String userValue = slugify(item.user?.interestVideo ?? '');
+      String memberValue = slugify(item.member.interestVideo ?? '');
+      if (isMatchOnly != true || userValue == memberValue) {
+        options.add({
+          'id': 'interestVideo',
+          'value': member.interestVideo as String,
+          'bg': colorSeondary,
+          'fg': colorBlack,
+        });
+      }
     }
     if (member.interestMusic != null) {
-      options.add({
-        'id': 'interestMusic',
-        'value': member.interestMusic as String,
-        'bg': colorSeondary200,
-        'fg': colorBlack,
-      });
+      String userValue = slugify(item.user?.interestMusic ?? '');
+      String memberValue = slugify(item.member.interestMusic ?? '');
+      if (isMatchOnly != true || userValue == memberValue) {
+        options.add({
+          'id': 'interestMusic',
+          'value': member.interestMusic as String,
+          'bg': colorSeondary200,
+          'fg': colorBlack,
+        });
+      }
     }
     if (member.interestTravelling != null) {
-      options.add({
-        'id': 'interestTravelling',
-        'value': member.interestTravelling as String,
-        'bg': colorSeondary200,
-        'fg': colorBlack,
-      });
+      String userValue = slugify(item.user?.interestTravelling ?? '');
+      String memberValue = slugify(item.member.interestTravelling ?? '');
+      if (isMatchOnly != true || userValue == memberValue) {
+        options.add({
+          'id': 'interestTravelling',
+          'value': member.interestTravelling as String,
+          'bg': colorSeondary200,
+          'fg': colorBlack,
+        });
+      }
     }
     if (member.interestPet != null) {
-      options.add({
-        'id': 'interestPet',
-        'value': member.interestPet as String,
-        'bg': colorSeondary200,
-        'fg': colorBlack,
-      });
+      String userValue = slugify(item.user?.interestPet ?? '');
+      String memberValue = slugify(item.member.interestPet ?? '');
+      if (isMatchOnly != true || userValue == memberValue) {
+        options.add({
+          'id': 'interestPet',
+          'value': member.interestPet as String,
+          'bg': colorSeondary200,
+          'fg': colorBlack,
+        });
+      }
     }
 
     return Div(
