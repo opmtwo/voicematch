11bf461bf1d19aeee907fa25548b097d41650631
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2023-07-14 19:57:43 +0530

    Updated ConnectionPic to support displaying self

--- lib/components/connection_pic.dart
+++ lib/components/connection_pic.dart
@@ -5,37 +5,43 @@ import 'package:voicematch/elements/div.dart';
 
 class ConnectionPic extends StatelessWidget {
   final ConnectionModel item;
+
   final double? w;
   final double? h;
+
+  final bool? isUser;
 
   const ConnectionPic({
     Key? key,
     required this.item,
     this.w,
     this.h,
+    this.isUser,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final UserProfileModel? profile = isUser == true ? item.user : item.member;
+
     return Div(
       [
-        if (item.member.pictureNormal != null)
+        if (profile?.pictureNormal != null)
           ProfilePic(
-            src: item.member.pictureNormal as String,
+            src: profile?.pictureNormal as String,
             isLocal: false,
             w: w,
             h: h,
           ),
-        if (item.member.pictureNormal?.isEmpty == true &&
-            item.member.pictureMasked != null)
+        if (profile?.pictureNormal?.isEmpty == true &&
+            profile?.pictureMasked != null)
           ProfilePic(
-            src: item.member.pictureMasked as String,
+            src: profile?.pictureMasked as String,
             isLocal: false,
             w: w,
             h: h,
           ),
-        if (item.member.pictureNormal?.isEmpty == true &&
-            item.member.pictureMasked?.isEmpty == true)
+        if (profile?.pictureNormal?.isEmpty == true &&
+            profile?.pictureMasked?.isEmpty == true)
           ProfilePic(
             src: 'assets/images/avatar.png',
             isLocal: true,
