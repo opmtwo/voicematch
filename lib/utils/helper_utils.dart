08937b809da6c638307354b844b2c10369a21091
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2023-07-21 13:08:22 +0530

    Added helper notifyError

--- lib/utils/helper_utils.dart
+++ lib/utils/helper_utils.dart
@@ -1,3 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:voicematch/constants/colors.dart';
+import 'package:voicematch/constants/theme.dart';
+
 String slugify(String text) {
   final pattern = RegExp(r'[^a-zA-Z0-9]+');
   final slug = text.toLowerCase().replaceAll(pattern, '-');
@@ -6,4 +11,19 @@ String slugify(String text) {
 
 String enumToString(Enum e) {
   return e.toString().split('.').last;
+}
+
+Future<void> notifyError(String title, String message) async {
+  Get.snackbar(
+    'Error',
+    'Message could not be send. Please try again.',
+    snackPosition: SnackPosition.BOTTOM,
+    backgroundColor: colorPrimary.withOpacity(
+      0.85,
+    ),
+    colorText: colorWhite,
+    padding: const EdgeInsets.all(
+      gap,
+    ),
+  );
 }
