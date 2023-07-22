03fbcbb8644dce43dc5b045b904f29068b6d24d1
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2023-07-22 11:17:00 +0530

    Added iconGallery

---
+++ lib/icons/icon_gallery.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:voicematch/utils/color_utils.dart';
+
+String iconGallery({Color? code = const Color(0xFF000000)}) {
+  return ("""
+<svg viewBox="0 0 24 24" fill="none">
+  <path d="M22 6C22 4.93913 21.5786 3.92172 20.8284 3.17157C20.0783 2.42143 19.0609 2 18 2H6C4.93913 2 3.92172 2.42143 3.17157 3.17157C2.42143 3.92172 2 4.93913 2 6V18C2 19.0609 2.42143 20.0783 3.17157 20.8284C3.92172 21.5786 4.93913 22 6 22H18C19.0609 22 20.0783 21.5786 20.8284 20.8284C21.5786 20.0783 22 19.0609 22 18V6Z" stroke="black" stroke-opacity="0.38" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M15.5 11C14.1193 11 13 9.88071 13 8.5C13 7.11929 14.1193 6 15.5 6C16.8807 6 18 7.11929 18 8.5C18 9.88071 16.8807 11 15.5 11Z" stroke="black" stroke-opacity="0.38" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M9.474 12.6213L18 22.0003H5.867C4.84141 22.0003 3.85782 21.5929 3.13262 20.8677C2.40741 20.1425 2 19.1589 2 18.1333V18.0003C2 17.5343 2.175 17.3553 2.49 17.0103L6.52 12.6153C6.70782 12.4104 6.93629 12.2469 7.19083 12.1353C7.44536 12.0236 7.72038 11.9662 7.99832 11.9668C8.27627 11.9674 8.55105 12.0259 8.80513 12.1385C9.05921 12.2512 9.28701 12.4157 9.474 12.6213V12.6213Z" stroke="black" stroke-opacity="0.38" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+""");
+}
