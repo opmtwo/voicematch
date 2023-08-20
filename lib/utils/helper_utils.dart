d9bd1fa9f7825ebb9b4c337b2cad2adfcceea3ef
Sam - OnePunchManTwo <76444410+opmtwo@users.noreply.github.com>
2023-08-21 00:28:21 +0530

    Added helper util getImageMimeType

--- lib/utils/helper_utils.dart
+++ lib/utils/helper_utils.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:voicevibe/constants/colors.dart';
 import 'package:voicevibe/constants/theme.dart';
@@ -26,4 +27,50 @@ Future<void> notifyError(String title, String message) async {
       gap,
     ),
   );
+}
+
+// Helper function to get the MIME type of an image based on its file name
+Future<String?> getImageMimeType(String fileName) async {
+  // Get the file extension from the given file name
+  String fileExtension = fileName.split('.').last.toLowerCase();
+
+  // Create a map of common image file extensions and their corresponding MIME types
+  Map<String, String> imageMimeTypes = {
+    'jpg': 'image/jpeg',
+    'jpeg': 'image/jpeg',
+    'png': 'image/png',
+    'gif': 'image/gif',
+    'bmp': 'image/bmp',
+    // Add more extensions and MIME types as needed
+  };
+
+  // Check if the file extension exists in the map
+  if (imageMimeTypes.containsKey(fileExtension)) {
+    return imageMimeTypes[fileExtension];
+  } else {
+    // If the extension is not found in the map, use platform method to get MIME type
+    try {
+      final data = await rootBundle.load(fileName);
+      final buffer = data.buffer.asUint8List();
+      final magicBytes = buffer.length >= 4 ? buffer.sublist(0, 4) : [];
+
+      if (magicBytes.length == 4) {
+        // Compare the magic bytes to identify the MIME type
+        if (magicBytes[0] == 0x89 &&
+            magicBytes[1] == 0x50 &&
+            magicBytes[2] == 0x4E &&
+            magicBytes[3] == 0x47) {
+          return 'image/png';
+        } else if (magicBytes[0] == 0xFF && magicBytes[1] == 0xD8) {
+          return 'image/jpeg';
+        }
+        // Add more checks for other magic bytes if needed
+      }
+    } catch (e) {
+      print('Error loading file: $e');
+    }
+  }
+
+  // Return a default MIME type if unable to identify
+  return 'application/octet-stream';
 }
